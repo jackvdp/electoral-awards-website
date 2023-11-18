8ccf9638f74b1f2f80c33ed7a915a22740d07c59
jackvdp <jack@pumpymusic.co.uk>
2023-11-18 12:38:08 +0000

    Add static square images

---
+++ pages/gallery.tsx
@@ -0,0 +1,46 @@
+import { NextPage } from 'next';
+import { Fragment } from 'react';
+// -------- custom component -------- //
+import { Navbar } from 'components/blocks/navbar';
+import { Footer } from 'components/blocks/footer';
+import SimpleBanner from 'components/blocks/banner/SimpleBanner';
+import useLightBox from 'hooks/useLightBox';
+import ImageCard from 'components/blocks/gallery/ImageCard';
+
+const Gallery: NextPage = () => {
+    useLightBox();
+
+    return (
+        <Fragment>
+            <Navbar />
+
+            <SimpleBanner title={"Gallery"}></SimpleBanner>
+
+            <main className="content-wrapper">
+
+                <section className="wrapper bg-light px-md-20 px-2 py-md-10 py-5 container">
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="row gy-6">
+
+                                <ImageCard name='alberto.jpg'/>
+
+                                <ImageCard name='barros.jpg'/>
+
+                                <ImageCard name='bruter.jpg'/>
+
+                                <ImageCard name='hicks.jpg'/>                                
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </main>
+
+            <Footer />
+        </Fragment>
+    );
+};
+
+export default Gallery;
+
---
+++ src/components/blocks/gallery/ImageCard.tsx
@@ -0,0 +1,31 @@
+import styles from './ImageCard.module.css';
+
+const ImageCard: React.FC<ImageCardProps> = ({ name }) => {
+    return (
+        <div className="col-md-4">
+            <div className={styles.squareContainer}>
+                <figure className={`overlay overlay-1 hover-scale rounded ${styles.figure}`}>
+                    <a
+                        href={`/img/judges/${name}`}
+                        data-gallery="gallery-image"
+                        data-glightbox=""
+                    >
+                        <img 
+                            src={`/img/judges/${name}`} 
+                            srcSet={`/img/judges/${name}`} 
+                            alt=""
+                        />
+                        <span className="bg" />
+                    </a>
+                </figure>
+            </div>
+        </div>
+    )
+}
+
+
+export default ImageCard
+
+interface ImageCardProps {
+    name: string;
+}
