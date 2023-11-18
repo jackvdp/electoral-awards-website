6b287ef26319057506de49d5b1c382ab33adbbc8
jackvdp <jack@pumpymusic.co.uk>
2023-11-18 16:10:00 +0000

    Get api working

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -4,14 +4,14 @@ import { getFileNames } from 'aws/aws';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const bucketName = 'electoralwebsite';
-    const folderName = 'awards_2023_photos'; // Replace or remove if not needed
+    const folderName = 'awards_2023_photos';
 
     try {
         const imageKeys = await getFileNames(bucketName, folderName);
-        const imageUrls = imageKeys.map(key => `https://${bucketName}.s3.your-region.amazonaws.com/${key}`);
+        const imageUrls = imageKeys.map(key => `https://${bucketName}.s3.eu-west-2.amazonaws.com/${key}`);
         res.status(200).json({ images: imageUrls });
     } catch (error) {
         console.error('Error in API:', error);
-        res.status(500).json({ error: 'Failed to fetch images' });
+        res.status(500).json({ error: 'Failed to fetch images: ' + error });
     }
 }
