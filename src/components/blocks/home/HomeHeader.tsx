e401b0962e0f01bd742446e00d2f7e4962b29bed
jackvdp <jack@pumpymusic.co.uk>
2023-12-27 19:41:20 +0000

    Use apos and link for linting

--- src/components/blocks/home/HomeHeader.tsx
+++ src/components/blocks/home/HomeHeader.tsx
@@ -1,4 +1,5 @@
 import NextLink from 'components/reuseable/links/NextLink'
+import Link from 'next/link'
 
 const HomeHeader: React.FC = () => {
     return (
@@ -9,13 +10,13 @@ const HomeHeader: React.FC = () => {
             <div className="container pt-10 pt-md-20 pb-10 pb-md-20 text-center">
                 <div className="row">
                     <div className="col-md-10 col-lg-8 col-xl-7 col-xxl-6 mx-auto">
-                        <h1 className="display-1 text-white mb-3">Electoral Stakeholders' Network</h1>
+                        <h1 className="display-1 text-white mb-3">Electoral Stakeholders&apos; Network</h1>
                         <p className="lead fs-lg px-md-3 px-lg-7 px-xl-9 px-xxl-10">
                             Connecting leaders in Electoral Management
                         </p>
                         <NextLink title="Register" href="/register" className="btn btn-soft-blue rounded-pill mt-2" />
                         <p className="lead fs-md px-md-3 px-lg-7 px-xl-9 px-xxl-10 py-2">
-                            <i>Already a a member? <a className='hover more' href="/register">Sign in</a></i>
+                            <i>Already a a member? <Link className='hover more' href="/register">Sign in</Link></i>
                         </p>
                     </div>
                 </div>
