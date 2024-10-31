64d6dbf8a9eab19763264cdaa58944b77bb3aa6b
jackvdp <jack@pumpymusic.co.uk>
2024-10-31 10:43:30 +0000

    Fix build error

--- src/components/blocks/home/HomeAbout.tsx
+++ src/components/blocks/home/HomeAbout.tsx
@@ -6,7 +6,7 @@ const HomeAbout: React.FC = () => {
             <h3 className="display-4 mb-3">Global Network for Electoral Excellence</h3>
             <p className="lead fs-lg lh-sm">Connecting Electoral Leaders, Sharing Knowledge, and Shaping the Future of Elections.</p>
             <p className="mb-6">
-                The Electoral Stakeholders' Network brings together electoral leaders, experts, and innovators from around the world. Through the prestigious International Electoral Awards, symposiums, interactive webinars, and a range of articles, the network promotes collaboration and knowledge-sharing among professionals committed to advancing electoral practices.
+                The Electoral Stakeholders&apos; Network brings together electoral leaders, experts, and innovators from around the world. Through the prestigious International Electoral Awards, symposiums, interactive webinars, and a range of articles, the network promotes collaboration and knowledge-sharing among professionals committed to advancing electoral practices.
                 <br /><br />
                 With a focus on critical topics—ranging from electoral integrity and technological advancements to inclusive participation and democratic engagement—the network provides opportunities for meaningful dialogue and practical solutions to evolving electoral challenges.
                 <br /><br />
