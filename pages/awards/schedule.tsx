77f8952d3d790746dea9431b0f3b27c16b240418
jackvdp <jack@pumpymusic.co.uk>
2024-12-15 16:09:26 -0400

    Refactor file

--- pages/awards/schedule.tsx
+++ pages/awards/schedule.tsx
@@ -1,21 +1,21 @@
-import { NextPage } from 'next';
-import { Fragment } from 'react';
+import {NextPage} from 'next';
+import {Fragment} from 'react';
 // -------- custom component -------- //
-import { Navbar } from 'components/blocks/navbar';
-import { Footer } from 'components/blocks/footer';
-import { schedule, Day } from 'data/schedule';
+import {Navbar} from 'components/blocks/navbar';
+import {Footer} from 'components/blocks/footer';
+import {schedule, Day} from 'data/schedule';
 import SimpleBanner from 'components/blocks/banner/SimpleBanner';
-import { TabBarAndContent } from 'components/reuseable/TabBar';
+import {TabBarAndContent} from 'components/reuseable/TabBar';
 import PageProgress from 'components/common/PageProgress';
 
 const Programme: NextPage = () => {
     return (
         <Fragment>
-            <PageProgress />
+            <PageProgress/>
 
-            <Navbar />
+            <Navbar/>
 
-            <SimpleBanner title={"Schedule"} />
+            <SimpleBanner title={"Schedule"}/>
 
             <main className="content-wrapper">
                 <section className="container wrapper bg-light px-2 py-md-10 py-5 ">
@@ -23,14 +23,14 @@ const Programme: NextPage = () => {
                     <TabBarAndContent items={
                         schedule.map(day => ({
                             title: day.title.split(':')[0],
-                            content: <DayCard day={day} />,
-                            icon: <i className="uil uil-calendar-alt pe-1" />
+                            content: <DayCard day={day}/>,
+                            icon: <i className="uil uil-calendar-alt pe-1"/>
                         }))
-                    } />
+                    }/>
                 </section>
             </main>
 
-            <Footer />
+            <Footer/>
         </Fragment>
     );
 };
@@ -41,34 +41,34 @@ type DayCardProps = {
     day: Day;
 };
 
-const DayCard: React.FC<DayCardProps> = ({ day }) => {
+const DayCard: React.FC<DayCardProps> = ({day}) => {
     return (
         <div className="mb-3">
-            <DayContent day={day} cardEnabled={true} />
-            <DayContent day={day} cardEnabled={false} />
+            <DayContent day={day} cardEnabled={true}/>
+            <DayContent day={day} cardEnabled={false}/>
         </div>
     );
 };
 
-function DayContent({ day, cardEnabled }: { day: Day, cardEnabled: boolean }) {
+function DayContent({day, cardEnabled}: { day: Day, cardEnabled: boolean }) {
     return (
         <div className={cardEnabled ? "d-none d-md-block card" : "d-md-none"}>
             <div className={cardEnabled ? "card-body" : ""}>
                 <h5 className={cardEnabled ? "card-title" : ""}>{day.title}</h5>
                 <table className="table table-striped">
                     <thead>
-                        <tr>
-                            <th className='w-15'>Time</th>
-                            <th>Event</th>
-                        </tr>
+                    <tr>
+                        <th className='w-15'>Time</th>
+                        <th>Event</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        {day.events.map((event, index) => (
-                            <tr key={index}>
-                                <td className='w-15'>{event.time}</td>
-                                <td dangerouslySetInnerHTML={{ __html: event.description }}></td>
-                            </tr>
-                        ))}
+                    {day.events.map((event, index) => (
+                        <tr key={index}>
+                            <td className='w-15'>{event.time}</td>
+                            <td dangerouslySetInnerHTML={{__html: event.description}}></td>
+                        </tr>
+                    ))}
                     </tbody>
                 </table>
             </div>
