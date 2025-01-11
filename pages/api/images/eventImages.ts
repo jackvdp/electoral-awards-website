ce6a6118b55718ec1f8f15be6a8bda187c19c77f
jackvdp <jack@pumpymusic.co.uk>
2025-01-11 22:31:15 +0000

    Fix build errors

--- pages/api/images/eventImages.ts
+++ pages/api/images/eventImages.ts
@@ -1,16 +1,14 @@
 // pages/api/eventImages.ts
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getFileAndFolderNames } from 'backend/helpers/aws/getFilesFolders';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {getFileAndFolderNames} from 'backend/helpers/aws/getFilesFolders';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const bucketName = 'electoralwebsite';
-    const folderName = 'photos';
 
     try {
-        const imageKeys = await getFileAndFolderNames(bucketName, folderName);
+        const imageKeys = await getFileAndFolderNames();
         res.status(200).json(imageKeys);
     } catch (error) {
         console.error('Error in API:', error);
-        res.status(500).json({ error: 'Failed to fetch images: ' + error });
+        res.status(500).json({error: 'Failed to fetch images: ' + error});
     }
 }
