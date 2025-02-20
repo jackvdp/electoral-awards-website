11d6f8970f69fd8b7b650c6b96cc6b276d159f31
jackvdp <jack@pumpymusic.co.uk>
2025-02-20 14:29:36 +0000

    Change hoeader to make it left aligned

--- src/components/blocks/home/HomeHeader.tsx
+++ src/components/blocks/home/HomeHeader.tsx
@@ -20,7 +20,7 @@ const HomeHeader = () => {
     ];
 
     return (
-        <section className={styles.headerSection + " text-white"}>
+        <section className={`${styles.headerSection} text-white`}>
             {/* Swiper Background */}
             <Swiper
                 modules={[Autoplay, EffectFade]}
@@ -55,62 +55,48 @@ const HomeHeader = () => {
 
             {/* Content */}
             <div className={styles.contentWrapper}>
-                <div className="container pt-10 pt-md-20 pb-10 pb-md-20 text-center">
+                <div className="container pt-10 pt-md-20 pb-10 pb-md-20">
                     <div className="row">
-                        <div className="col-md-10 col-lg-8 col-xl-7 col-xxl-6 mx-auto">
-                            <h1 className="display-1 text-white mb-3">
+                        <div className="col-md-8 col-lg-7 col-xl-6 col-xxl-6">
+                            <h1 className="display-1 text-white mb-3 text-start">
                                 Electoral Stakeholders&apos; Network
                             </h1>
-                            <p className="lead fs-lg px-md-3 px-lg-7 px-xl-9 px-xxl-10">
+                            <p className="lead fs-lg text-start">
                                 Connecting Leaders in Electoral Management
                             </p>
 
+                            <p className="text-start">
+                                The Electoral Stakeholders&apos; Network connects electoral professionals, policymakers,
+                                and
+                                innovators to strengthen democracy worldwide. Through symposiums, research, and awards,
+                                we foster knowledge-sharing, international cooperation, and solutions for electoral
+                                integrity, transparency, and voter participation.
+                            </p>
+
+                            <div className="d-flex gap-2">
+                                <NextLink
+                                    title="Register"
+                                    href="/register"
+                                    className="btn btn-soft-blue rounded-pill mt-2"
+                                />
+                                <NextLink
+                                    title="Awards"
+                                    href="/awards"
+                                    className="btn btn-blue rounded-pill mt-2"
+                                />
+                            </div>
+
                             {!isLoggedIn && (
-                                <>
-                                    <div className="d-flex justify-content-center gap-2">
-                                        <NextLink
-                                            title="Register"
-                                            href="/register"
-                                            className="btn btn-soft-blue rounded-pill mt-2"
-                                        />
-                                        <NextLink
-                                            title="Awards"
-                                            href="/awards"
-                                            className="btn btn-blue rounded-pill mt-2"
-                                        />
-                                    </div>
-
-                                    <p className={`lead fs-md px-md-3 px-lg-7 px-xl-9 px-xxl-10 py-4 ${styles.subtitleText}`}>
-                                        Already a member?{' '}
-                                        <a
-                                            data-bs-toggle="modal"
-                                            data-bs-target="#modal-signin"
-                                            className="hover more link-aqua"
-                                        >
-                                            Sign in
-                                        </a>
-                                    </p>
-                                </>
-                            )}
-
-                            {isLoggedIn && (
-                                <div className="d-flex justify-content-center gap-2">
-                                    <NextLink
-                                        title="Events"
-                                        href="/events"
-                                        className="btn btn-soft-blue rounded-pill mt-2"
-                                    />
-                                    <NextLink
-                                        title="Articles"
-                                        href="/articles"
-                                        className="btn btn-outline-white rounded-pill mt-2"
-                                    />
-                                    <NextLink
-                                        title="Awards"
-                                        href="/awards"
-                                        className="btn btn-yellow rounded-pill mt-2"
-                                    />
-                                </div>
+                                <p className={`lead fs-md py-4 text-start ${styles.subtitleText}`}>
+                                    Already a member?{' '}
+                                    <a
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#modal-signin"
+                                        className="hover more link-aqua"
+                                    >
+                                        Sign in
+                                    </a>
+                                </p>
                             )}
                         </div>
                     </div>
