249980970247cd2502ea15b47aff02bc43d5ec33
jackvdp <jack@pumpymusic.co.uk>
2025-03-16 22:06:05 +0000

    If booking already exists but it's not accepted then we just update to accept

--- src/backend/use_cases/bookings/createBooking.ts
+++ src/backend/use_cases/bookings/createBooking.ts
@@ -1,22 +1,37 @@
 import Booking, {IBooking} from "backend/models/booking";
+import {updateBooking} from "./updateBooking";
 
 export async function createBooking({userId, eventId}: {userId: string, eventId: string}): Promise<IBooking> {
     try {
         const existingBooking = await Booking.findOne({ userId, eventId });
 
         if (existingBooking) {
-            throw new Error('User already has a booking for this event');
+
+            if (existingBooking.status === 'accepted') {
+                throw new Error('User already has a booking for this event');
+            }
+
+            const result = await updateBooking({
+                bookingId: existingBooking._id.toString(),
+                status: 'accepted'
+            });
+
+            if (result.success && result.booking) {
+                return result.booking;
+            } else {
+                throw new Error('Failed to update existing booking');
+            }
+        } else {
+            const booking = new Booking ({
+                userId,
+                eventId,
+                status: 'accepted',
+            });
+
+            await booking.save();
+
+            return booking;
         }
-
-        const booking = new Booking ({
-            userId,
-            eventId,
-            status: 'accepted',
-        });
-
-        await booking.save();
-
-        return booking;
     } catch (error) {
         console.error('Error creating booking:', error);
         throw new Error('Failed to create booking');
