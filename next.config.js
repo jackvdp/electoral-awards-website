2ccd894a536d659f172a6843d4d6d7c476f13059
jackvdp <jack@pumpymusic.co.uk>
2025-03-31 19:00:54 +0100

    Add domain for image

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
             'electoralwebsite.s3.eu-west-2.amazonaws.com',
             'electoralwebsite.s3.amazonaws.com',
             '4y9kpertpu07sqtv.public.blob.vercel-storage.com',
-            't4.ftcdn.net'
+            't4.ftcdn.net',
+            'as2.ftcdn.net/'
         ],
     }
 }
